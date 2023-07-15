c58682f2f48d6b87694e911112557f429cdfb790
Aigerim Abitayeva <96001315+abitoffka@users.noreply.github.com>
2023-07-16 02:13:51 +0600

    Add files via upload

---
+++ magic-coffee/magic-coffee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  magic-coffee
+//
+//  Created by Aigerim Abitayeva on 16.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
